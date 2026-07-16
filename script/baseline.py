9605c359b8c854766af9c33e8ac5b3d7b7b527de
astral-fi <ashutosh24135@iiitd.ac.in>
2026-07-16 20:51:14 +0530

    Implemented Baseline

---
+++ script/baseline.py
@@ -0,0 +1,82 @@
+from tokenizer import PointCloudTokenizer as Tokenizer
+from dataset import DATASET_PATH, concatenate_h5_files, ModelNet40Dataset
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+
+data_train, label_train = concatenate_h5_files(DATASET_PATH, 5, 'train')
+data_test, label_test = concatenate_h5_files(DATASET_PATH, 2, 'test')
+
+dataset_train = ModelNet40Dataset(data_train, label_train, 1024)
+dataset_train_loader = DataLoader(dataset_train, batch_size=64, shuffle=True)
+dataset_test = ModelNet40Dataset(data_test, label_test, 1024)
+dataset_test_loader = DataLoader(dataset_test, batch_size=64, shuffle=False)
+print(f"Training dataset size: {len(dataset_train)}, Testing dataset size: {len(dataset_test)}")
+
+
+class BaselineModel(nn.Module):
+    def __init__(self, tokenizer):
+        super(BaselineModel, self).__init__()
+        self.tokenizer = tokenizer
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=128, nhead=4, dim_feedforward=512, batch_first=True, activation='gelu')
+        self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=4)
+
+        self.linear_head = nn.Linear(tokenizer.token_dim, 40)  # Assuming 40 classes for ModelNet40
+
+    def forward(self, x):
+        tokens, positional_embeddings = self.tokenizer(x)
+        combined_tokens = tokens + positional_embeddings  # Combine tokens with positional embeddings
+        x = self.encoder(combined_tokens)
+        x = torch.mean(x, dim=1)  # Global average pooling over the sequence length
+        x = self.linear_head(x)
+        return x
+
+
+if __name__ == "__main__":
+    tokenizer = Tokenizer(num_samples=64, k_neighbors=32, token_dim=128)
+    tokenizer.to('cuda' if torch.cuda.is_available() else 'cpu')  # Move the model to GPU if available
+    model = BaselineModel(tokenizer)
+    model.to('cuda' if torch.cuda.is_available() else 'cpu')  # Move the model to GPU if available
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, weight_decay=1e-4)
+    criterion = nn.CrossEntropyLoss()
+    EPOCHS = 20
+
+    for epoch in range(EPOCHS):
+        train_loss = 0.0
+
+        for points, labels in dataset_train_loader:
+
+            points = points.to('cuda' if torch.cuda.is_available() else 'cpu')
+            labels = labels.to('cuda' if torch.cuda.is_available() else 'cpu')
+
+            logits = model(points)
+
+            loss = criterion(logits, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            train_loss += loss.item()
+
+        print(f"Epoch [{epoch+1}/{EPOCHS}], Loss: {train_loss/len(dataset_train_loader):.4f}")
+
+        with torch.no_grad():
+            model.eval()
+            correct = 0
+            total = 0
+
+            for points, labels in dataset_test_loader:
+                points = points.to('cuda' if torch.cuda.is_available() else 'cpu')
+                labels = labels.to('cuda' if torch.cuda.is_available() else 'cpu')
+
+                logits = model(points)
+                _, predicted = torch.max(logits.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+            accuracy = 100 * correct / total
+            print(f"Test Accuracy: {accuracy:.2f}%")
