d9d8a77f58d26b7255864219b7f007ca751f7318
astral-fi <ashutosh24135@iiitd.ac.in>
2026-07-17 20:58:02 +0530

    Fixed Dataset bug

--- script/dataset.py
+++ script/dataset.py
@@ -39,7 +39,7 @@ def concatenate_h5_files(dataset_path, num_files=5, dataset_type='train'):
     label = np.concatenate(label, axis=0)
     return data, label
 
-def rotate_point_cloud(points, axis='y'):
+def rotate_point_cloud(points, axis='z'):
     theta = np.random.uniform(0, 2 * np.pi)
     cos_t, sin_t = np.cos(theta), np.sin(theta)
 
